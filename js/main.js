d314fb19069b3f42b43c15a4645f8b5cc28fa068
kwabsntim <kwabsntim@gmail.com>
2026-09-16 14:46:09 +0000

    addition of comment

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const CONFIG = {
   TYPING_SPEED: 100,
   TYPING_TEXT: "🚚 Interesseret i vores service? Klik på 'Bestil nu' øverst!",
   MOBILE_BREAKPOINT: 768,
+  //api endpoint for the code
   API_ENDPOINT: 'https://ihb-transport-1.onrender.com/api/public/deliveries'
 };
 
